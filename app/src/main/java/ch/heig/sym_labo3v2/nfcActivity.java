e7a98837dc93cd2c25e790e2100b3ea3ad0b31b2
Nicolas Rod <ncls.rod@gmail.com>
2017-12-11 15:51:08 +0100

    Wip nfc activity

--- app/src/main/java/ch/heig/sym_labo3v2/nfcActivity.java
+++ app/src/main/java/ch/heig/sym_labo3v2/nfcActivity.java
@@ -1,13 +1,45 @@
 package ch.heig.sym_labo3v2;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class nfcActivity extends AppCompatActivity {
+
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+
+        mTextView = (TextView) findViewById(R.id.textViewExplanation);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText("NFC is enabled.");
+        }
+
+        handleIntent(getIntent());
+    }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
     }
 }
