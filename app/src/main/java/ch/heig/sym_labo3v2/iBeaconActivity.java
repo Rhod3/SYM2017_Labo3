c59c3e067f74139156dfd89d67d2bc81f75057cd
Nadir Benallal <nadir.benallal@heig-vd.ch>
2017-12-11 15:51:27 +0100

    Permissions done. Testing ibeacon for the first time

--- app/src/main/java/ch/heig/sym_labo3v2/iBeaconActivity.java
+++ app/src/main/java/ch/heig/sym_labo3v2/iBeaconActivity.java
@@ -1,13 +1,186 @@
 package ch.heig.sym_labo3v2;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.RemoteException;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class iBeaconActivity extends AppCompatActivity {
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.List;
+
+public class iBeaconActivity extends AppCompatActivity implements BeaconConsumer {
+
+    private List<?> ibeacons;
+    private BeaconManager beaconManager;
+
+    private static String tagEnteringRegion = "TAG1";
+    private static String tagExitRegion = "TAG2";
+    private static String tagDetermineStateRegion = "TAG3";
+
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private TextView test = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_i_beacon);
+
+        //Creating the beacon manager
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+
+        //Checking prerequisites
+        askLocationPermission();
+        checkInternetConnection();
+        checkBluetoothStatus();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i(tagEnteringRegion, "I just saw an beacon for the first time!");
+                Toast.makeText(getBaseContext(), "I just saw an beacon for the first time!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(tagExitRegion, "I no longer see an beacon");
+                Toast.makeText(getBaseContext(), "I no longer see an beacon", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i(tagDetermineStateRegion, "I have just switched from seeing/not seeing beacons: "+state);
+                Toast.makeText(getBaseContext(), "I have just switched from seeing/not seeing beacons: "+state, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("TAG1000", "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle(R.string.location_permission_fail_title);
+                    builder.setMessage(R.string.location_permission_fail_message);
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager)getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnected();
+    }
+
+    private void askLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.location_permission_title);
+                builder.setMessage(R.string.location_permission_message);
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    @TargetApi(Build.VERSION_CODES.M)
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+    }
+
+    private void checkInternetConnection() {
+        if (!isConnected()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.internet_permission_title);
+            builder.setMessage(R.string.internet_permission_message);
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                @TargetApi(Build.VERSION_CODES.M)
+                public void onDismiss(DialogInterface dialogInterface) {
+                    Intent enableInternetIntent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                    startActivity(enableInternetIntent);
+                }
+            });
+            builder.show();
+        }
+    }
+
+    private void checkBluetoothStatus() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.bluetooth_permission_title);
+            builder.setMessage(R.string.bluetooth_permission_message);
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                @TargetApi(Build.VERSION_CODES.M)
+                public void onDismiss(DialogInterface dialogInterface) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            });
+            builder.show();
+        }
     }
 }
