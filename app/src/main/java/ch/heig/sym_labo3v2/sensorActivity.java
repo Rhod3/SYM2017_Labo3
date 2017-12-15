7916790133172b5434d4bdf4f9a9eccc3a97ca51
Basile portable <basile.chatillon@heig-vd.ch>
2017-12-15 12:36:13 +0100

    sensor ok

--- app/src/main/java/ch/heig/sym_labo3v2/sensorActivity.java
+++ app/src/main/java/ch/heig/sym_labo3v2/sensorActivity.java
@@ -1,13 +1,122 @@
 package ch.heig.sym_labo3v2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class sensorActivity extends AppCompatActivity {
+public class sensorActivity extends AppCompatActivity implements SensorEventListener {
+
+    //opengl
+    private OpenGLRenderer opglr = null;
+    private GLSurfaceView m3DView = null;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private Sensor magneticSensor;
+
+    private float[] lastRotMatrix = {0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f};
+
+    private float[] newRotMatix = {0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f,
+            0f, 0f, 0f, 0f};
+
+    private float[] gravity = null;
+    private float[] geoMagnetic = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // we need fullscreen
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // we initiate the view
         setContentView(R.layout.activity_sensor);
+
+        // link to GUI
+        this.m3DView = (GLSurfaceView) findViewById(R.id.compass_opengl);
+
+        //we create the 3D renderer
+        this.opglr = new OpenGLRenderer(getApplicationContext());
+
+        //init opengl surface view
+        this.m3DView.setRenderer(this.opglr);
+
+        // init sensor
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_GAME);
     }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // On commence par vérifier que le sensor est fiable
+        if (sensorEvent.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+            return;
+
+        /* On récupère le type d'événement dans un switch, et en fonction, on récupère les valeurs
+         * et on les met dans la bonne matrice */
+
+        switch (sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                this.gravity = sensorEvent.values;
+                break;
+
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                this.geoMagnetic = sensorEvent.values;
+                break;
+        }
+
+        if (gravity != null && geoMagnetic != null) {
+            // We do not need any inclination matrix
+
+            boolean ok = SensorManager.getRotationMatrix(newRotMatix, null, gravity, geoMagnetic);
+
+            if (ok) {
+                lastRotMatrix = this.opglr.swapRotMatrix(newRotMatix);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    /* TODO */
+    // your activity need to register accelerometerSensor and magnetometer sensors' updates
+    // then you may want to call
+    //  this.opglr.swapRotMatrix()
+    // with the 4x4 rotation matrix, everytime a new matrix is computed
+    // more information on rotation matrix can be found on-line:
+    // https://developer.android.com/reference/android/hardware/SensorManager.html#getRotationMatrix(float[],%20float[],%20float[],%20float[])
+
+
 }
